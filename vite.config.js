863007e5027132f5a4fb99546340ff831e743a2f
Shashivarunreddy <137184492+Shashivarunreddy@users.noreply.github.com>
2024-11-29 11:54:21 +0530

    Fix Rollup external dependencies

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,15 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@solana/wallet-adapter-base',
-        '@solana/wallet-adapter-react',
-        '@solana/wallet-adapter-react-ui',
-        '@solana/wallet-adapter-wallets',
-        '@solana/web3.js'
-      ]
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            external: [
+                '@solana/wallet-adapter-react',
+                '@solana/wallet-adapter-react-ui',
+                '@solana/wallet-adapter-wallets',
+                '@solana/web3.js'
+            ]
+        }
     }
-  }
 });
